c8a8b3ccce026d794b3d3d3743983720319b0e71
Pankaj Arun Meshram <mpankajarun@gmail.com>
2017-04-27 12:39:01 +0530

    Configured app and testing django app

--- CywareAssignment/settings.py
+++ CywareAssignment/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app', # registering the application
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +101,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+# configure paths for our Static files (CSS, JavaScript, images, etc) and the path to our template files.
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
