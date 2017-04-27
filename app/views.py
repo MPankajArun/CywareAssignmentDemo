d45799ad09d7c8a65becdd11f1fd3b7b30579a82
Pankaj Arun Meshram <mpankajarun@gmail.com>
2017-04-27 15:00:38 +0530

    Designed a profile.html page to view user data from GitHub

--- app/views.py
+++ app/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render, HttpResponse
+import requests
+import json
 
 
 # Create your views here.
 
 def index(request):
     return HttpResponse('Testing View')
+
+
+def profile(request):
+    req = requests.get('https://api.github.com/users/mpankajarun')
+    #content = req.text
+    jsonList = []
+    jsonList.append(json.loads(req.content))
+    content = []
+    userData = {}
+    for data in jsonList:
+        userData['name'] = data['name']
+        userData['company'] = data['company']
+        userData['location'] = data['location']
+        userData['blog'] = data['blog']
+        userData['email'] = data['email']
+        userData['public_gists'] = data['public_gists']
+        userData['public_repos'] = data['public_repos']
+        userData['avatar_url'] = data['avatar_url']
+        userData['followers'] = data['followers']
+        userData['following'] = data['following']
+    content.append(userData)
+    #return HttpResponse(content)
+    return render(request, 'app/profile.html', {'data': content})
